f5b6699973d2eb8411f4dbc4a1c2d3c71ad06dcd
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-07 08:37:27 -0400

    basic server and gitignore

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+// Data
+
+
+// Routes
+
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+})
