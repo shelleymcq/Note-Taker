55f435618cb0e083d6d940b8b9add03723898a45
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-07 09:50:12 -0400

    added html and api routes

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 // Dependencies
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
+
 
 const app = express();
-const PORT = 4000;
-
-// Data
 
 
 // Routes
 
+app.get('/test', (req, res) => res.sendFile(path.join(__dirname, 'public/test.html')));
+
+// '/' works, '*' blocks the notes res, but not the test res????
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/db', (req, res) => res.json(db));
+
+
+
+const PORT = process.env.PORT || 4000;
 
 // Listener
 app.listen(PORT, () => {
