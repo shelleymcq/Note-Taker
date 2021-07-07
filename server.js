842ad6fcb806a7f205496264a249041780b06512
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-07 11:11:33 -0400

    post receives a new note and saves on req.body

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const db = require('./db/db.json');
 
 
 const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 
 // Routes
@@ -15,13 +20,23 @@ app.get('/test', (req, res) => res.sendFile(path.join(__dirname, 'public/test.ht
 // '/' works, '*' blocks the notes res, but not the test res????
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/db', (req, res) => res.json(db));
+// reads the db.json file containing saved notes
+app.get('/api/notes', (req, res) => res.json(db));
+
+// create new note
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newNote title
+    newNote.title = newNote.title.replace(/\s+/g, '').toLowerCase();
+    console.log(newNote);
+    db.push(newNote);
+    res.json(newNote);
+})
 
 
-
-const PORT = process.env.PORT || 4000;
 
 // Listener
 app.listen(PORT, () => {
